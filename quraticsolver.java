25d1a1d899c86d50b782bbaa1d78770055e44c3d
Papa-DMan <66575559+Papa-DMan@users.noreply.github.com>
2023-10-19 16:13:33 -0400

    4, 6, 9

--- quraticsolver.java
+++ quraticsolver.java
@@ -3,7 +3,7 @@ public class quraticsolver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 3;
+		int a = 4;
 		int b = 6;
 		int c = 9;
 		if (b*b - 4*a*c <0)
